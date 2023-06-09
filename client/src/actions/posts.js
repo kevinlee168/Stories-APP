69bf883cb8889f174dfa8b194bd359734477585a
kevinlee168 <lisan521cool@gmail.com>
2023-06-09 21:51:34 +0800

    added constants for action types

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,13 +1,11 @@
+import { FETCH_ALL, CREATE, DELETE, UPDATE } from '../constants/actionTypes';
 import * as api from '../api';
 
 // Action Creators
 export const getPosts = () => async (dispatch) => {
     try {
         const { data } = await api.fetchPosts(); // via api to get posts
-        const action = {
-            type: 'FETCH_ALL',
-            payload: data
-        }
+        const action = { type: FETCH_ALL, payload: data }
 
         dispatch(action);  // to dispacth an action to the store
     } catch (error) {
@@ -19,7 +17,7 @@ export const createPost = (post) => async (dispatch) => {
     try {
         const { data } = await api.createPost(post);
 
-        dispatch({ type: 'CREATE', payload: data }); // const action = { type: 'CREATE', payload: data }
+        dispatch({ type: CREATE, payload: data }); // const action = { type: 'CREATE', payload: data }
     } catch (error) {
         console.log(error);
     }
@@ -30,7 +28,7 @@ export const updatePost = (id, post) => async (dispatch) => {
     try {
         const { data } = await api.updatePost(id, post);
 
-        dispatch({ type: 'UPDATE', payload: data });
+        dispatch({ type: UPDATE, payload: data });
     } catch (error) {
         console.log(error);
     }
@@ -40,7 +38,7 @@ export const deletePost = (id) => async (dispatch) => {
     try {
         await api.deletePost(id);
 
-        dispatch({ type: 'DELETE', payload: id });
+        dispatch({ type: DELETE, payload: id });
     } catch (error) {
         console.log(error);
     }
