c1c34cae5236b7584dd927b513841e3fa13c8573
kevinlee168 <lisan521cool@gmail.com>
2023-06-09 16:34:22 +0800

    update layout in diff devices

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { Container, Grow, Grid } from '@mui/material';
+import { Container, Grow, Grid, useMediaQuery, useTheme } from '@mui/material';
 import { useDispatch } from 'react-redux'; // The useDispatch hook lets React components dispatch actions to the store
 import { getPosts } from './actions/posts'
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import ResponsiveAppBar from './components/ResponsiveAppBar/ResponsiveAppBar';
+
+
 
 const App = () => {
     const [currentId, setCurrentId] = useState(0);
@@ -14,22 +16,26 @@ const App = () => {
         dispatch(getPosts());
     }, [dispatch, currentId]);
 
+    const theme = useTheme();
+    // const prefersMobile = useMediaQuery('(max-width: 600px)');
+    const prefersMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
     return (
         <Container maxWidth="lg">
             <ResponsiveAppBar />
             <Grow in>
                 <Container>
-                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                    <Grid container style={{ flexDirection: prefersMobile ? 'column-reverse' : 'row' }} justify="space-between" alignItems="stretch" spacing={3} >
                         <Grid item xs={12} sm={8}>
                             <Posts setCurrentId={setCurrentId} />
                         </Grid>
-                        <Grid item xs={12} sm={4}>
+                        <Grid item xs={12} sm={4}  >
                             <Form currentId={currentId} setCurrentId={setCurrentId} />
                         </Grid>
                     </Grid>
                 </Container>
-            </Grow>
-        </Container>
+            </Grow >
+        </Container >
     );
 }
 
