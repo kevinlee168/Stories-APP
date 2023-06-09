70524ab5f5a1580d48119bf3e2ddf7c5fad8f5e5
kevinlee168 <lisan521cool@gmail.com>
2023-06-09 21:54:54 +0800

    added constants for action types

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,14 +1,15 @@
-// It is a reducer
+import { FETCH_ALL, CREATE, DELETE, UPDATE } from '../constants/actionTypes';
+
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (posts = [], action) => {
     switch (action.type) {
-        case 'FETCH_ALL':
+        case FETCH_ALL:
             return action.payload; // return the data (posts)
-        case 'CREATE':
+        case CREATE:
             return [...posts, action.payload];
-        case 'UPDATE':
+        case UPDATE:
             return posts.map((post) => post._id === action.payload._id ? action.payload : post);
-        case 'DELETE':
+        case DELETE:
             return posts.filter((post) => post._id !== action.payload);
         default:
             return posts;
