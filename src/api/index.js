d08a6ec723b23c8500e3ee2b8c7630ea7dfdb067
kevinlee168 <lisan521cool@gmail.com>
2023-06-12 22:17:48 +0800

    update

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 // const url = 'http://localhost:5001/posts';
 const url = 'https://stories-app-api.herokuapp.com/posts';
 
+
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
 export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
